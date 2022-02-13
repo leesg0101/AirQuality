80b781dbd1092f6b7c897fe20bca8e037b5e2910
leesg0101 <leesg0101@gmail.com>
2022-02-13 23:31:00 +0900

    레트로핏으로 미세먼지 정보를 가져와 화면에 표

--- app/src/main/java/com/sg0101/app/airquality/MainActivity.kt
+++ app/src/main/java/com/sg0101/app/airquality/MainActivity.kt
@@ -19,10 +19,20 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.sg0101.app.airquality.databinding.ActivityMainBinding
+import com.sg0101.app.airquality.retrofit.AirQualityResponse
+import com.sg0101.app.airquality.retrofit.AirQualityService
+import com.sg0101.app.airquality.retrofit.RetrofitConnection
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.io.IOException
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 private const val TAG = "MainActivity"
+private const val KEY = "your API KEY"  // your API KEY
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,6 +55,7 @@ class MainActivity : AppCompatActivity() {
 
         checkAllPermissions()
         updateUI()
+        setRefreshButton()
     }
 
     override fun onRequestPermissionsResult(
@@ -156,6 +167,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvLocationSubtitle.text = "${it.countryName} ${it.adminArea}"
             }
             // 2. 현재 미세먼지 농도 가져오고 UI 업데이트
+            getAirQualityData(latitude, longitude)
         } else {
             Toast.makeText(this, "위도, 경도 정보를 가져올 수 없었습니다. 새로고침을 눌러주세요.", Toast.LENGTH_LONG).show()
         }
@@ -185,5 +197,77 @@ class MainActivity : AppCompatActivity() {
         }
 
         return addresses[0]
+    }
+
+    private fun getAirQualityData(latitude: Double, longitude: Double) {
+        // 레트로핏 객체를 이용해 AirQualityService 인터페이스 구현체를 가져올 수 있음
+        val retrofitAPI = RetrofitConnection.getInstance().create(AirQualityService::class.java)
+
+        retrofitAPI.getAirQualityData(
+            latitude.toString(),
+            longitude.toString(),
+            KEY
+        ).enqueue(object : Callback<AirQualityResponse> {
+            override fun onResponse(
+                call: Call<AirQualityResponse>,
+                response: Response<AirQualityResponse>
+            ) {
+                if (response.isSuccessful) {
+                    Toast.makeText(this@MainActivity, "최신 정보 업데이트 완료!", Toast.LENGTH_SHORT).show()
+                    response.body()?.let { updateAirUI(it) }
+                } else {
+                    Toast.makeText(this@MainActivity, "업데이트에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<AirQualityResponse>, t: Throwable) {
+                t.printStackTrace()
+                Toast.makeText(this@MainActivity, "업데이트에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun updateAirUI(airQualityData: AirQualityResponse) {
+        val pollutionData = airQualityData.data.current.pollution
+
+        // 수치 지정(메인 화면 가운데 숫자)
+        binding.tvCount.text = pollutionData.aqius.toString()
+
+        // 측정된 날짜 지정
+        val dateTime =
+            ZonedDateTime.parse(pollutionData.ts).withZoneSameInstant(ZoneId.of("Asia/Seoul"))
+                .toLocalDateTime()
+        val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+
+        binding.tvCheckTime.text = dateTime.format(dateFormatter).toString()
+
+        when(pollutionData.aqius) {
+            in 0..50 -> {
+                binding.tvTitle.text = "좋음"
+                binding.imgBg.setImageResource(R.drawable.bg_good)
+            }
+            in 51..150 -> {
+                binding.tvTitle.text = "보통"
+                binding.imgBg.setImageResource(R.drawable.bg_soso)
+            }
+            in 151..200 -> {
+                binding.tvTitle.text = "나쁨"
+                binding.imgBg.setImageResource(R.drawable.bg_soso)
+            }
+            else -> {
+                binding.tvTitle.text = "매우 나쁨"
+                binding.imgBg.setImageResource(R.drawable.bg_worst)
+            }
+        }
+
+        // geocoder 에서 주소 못 가져올 경우
+        binding.tvLocationTitle.text = airQualityData.data.city
+        binding.tvLocationSubtitle.text = airQualityData.data.country
+    }
+
+    private fun setRefreshButton() {
+        binding.btnRefresh.setOnClickListener {
+            updateUI()
+        }
     }
 }
