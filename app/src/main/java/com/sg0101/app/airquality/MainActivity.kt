d31fe63982871ee0d998fc60e9d378364f17f105
leesg0101 <leesg0101@gmail.com>
2022-02-13 19:01:31 +0900

    GPS 와 인터넷권한 설정

--- app/src/main/java/com/sg0101/app/airquality/MainActivity.kt
+++ app/src/main/java/com/sg0101/app/airquality/MainActivity.kt
@@ -1,11 +1,126 @@
 package com.sg0101.app.airquality
 
+import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.sg0101.app.airquality.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSION_REQUEST_CODE = 100
+    var REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    lateinit var getGPSPermissionLauncher: ActivityResultLauncher<Intent>
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        checkAllPermissions()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size) {
+            var checkResult = true
+
+            for (result in grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    checkResult = false
+                    break
+                }
+            }
+
+            if (checkResult) {
+
+            } else {
+                Toast.makeText(this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요", Toast.LENGTH_LONG)
+                    .show()
+                finish()
+            }
+        }
+    }
+
+    private fun checkAllPermissions() {
+        if (!isLocationServiceAvailable()) {
+            showDialogForLocationServiceSetting();
+        } else {
+            isRunTimePermissionsGranted()
+        }
+    }
+
+    private fun showDialogForLocationServiceSetting() {
+        getGPSPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (isLocationServiceAvailable()) {
+                    isRunTimePermissionsGranted()
+                } else {
+                    Toast.makeText(this, "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_LONG)
+                        .show()
+                    finish()
+                }
+            }
+        }
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("위치 서비스가 꺼져 있습니다. 설정해야 앱을 사용할 수 있습니다.")
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정", DialogInterface.OnClickListener { dialog, id ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            getGPSPermissionLauncher.launch(callGPSSettingIntent)
+        })
+        builder.setNegativeButton("취소", DialogInterface.OnClickListener { dialog, id ->
+            dialog.cancel()
+            Toast.makeText(this, "기기에서 위치서비스(GPS) 설정 후 사용해주세요.", Toast.LENGTH_LONG)
+                .show()
+            finish()
+        })
+        builder.create().show()
+    }
+
+    private fun isRunTimePermissionsGranted() {
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+            this@MainActivity,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            this@MainActivity, Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED ||
+            hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    private fun isLocationServiceAvailable(): Boolean {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
     }
 }
